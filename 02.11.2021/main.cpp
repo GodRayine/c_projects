d43c1de9e9b7fd79bf28e0f43ae9764cb6aebb06
GodRayine <NUB999999@gmail.com>
2021-11-03 00:25:13 +0300

    add settings and play ground

--- 02.11.2021/main.cpp
+++ 02.11.2021/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-
 using namespace std;
+
 void rules(){
     cout << "[*] для одиночной игры:\n\n"
             "[1] Вы вводите координту(x и y) того места куда хотите поставить значёк\n"
@@ -12,11 +12,21 @@ void rules(){
             "[3] Всё повторяеться до победы одной из сторон\n\n";
 }
 void settings(){
-
+    cout << "\tНастройки\n\n"
+            "[*] Выберите размер поля:\n"
+            "[1] 3x3\n"
+            "[2] 4x4\n"
+            "[3] 5x5\n";
 }
+
 
 int main() {
     setlocale(LC_ALL, "ru_RU.UTF-8");
+    int select = 1;
+    const int play_ground_one = 3;
+    const int play_ground_two = 4;
+    const int play_ground_three = 5;
+
     cout << "\tКрестики нолики\n\n"
             "[*] Меню:\n"
             "[1] Играть\n"
@@ -27,9 +37,32 @@ int main() {
     cin >> menu;
     switch (menu) {
         case 1:
-            break;
+            if (select == 1){
+                int play_ground[play_ground_one][play_ground_one] = {
+                        {1, 1, 1},
+                        {1, 1, 1},
+                        {1, 1, 1}
+                };
+            } else if (select == 2){
+                int play_ground[play_ground_two][play_ground_two] = {
+                        {1, 1, 1, 1},
+                        {1, 1, 1, 1},
+                        {1, 1, 1, 1},
+                        {1, 1, 1, 1}
+                };
+            } else if (select == 3){
+                int play_ground[play_ground_three][play_ground_three] = {
+                        {1, 1, 1, 1, 1},
+                        {1, 1, 1, 1, 1},
+                        {1, 1, 1, 1, 1},
+                        {1, 1, 1, 1, 1},
+                        {1, 1, 1, 1, 1}
+                };
+            }
         case 2:
-            break;
+            settings();
+            cin >> select;
+            main();
         case 3:
             rules();
             cout << "[!] что бы продолжить, введите любой сивыол и нажмите \"Enter\"";
