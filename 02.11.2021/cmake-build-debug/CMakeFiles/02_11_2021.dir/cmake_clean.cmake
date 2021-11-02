8e438123494c143449b9e3587f91873652eed8a0
GodRayine <NUB999999@gmail.com>
2021-11-02 15:43:14 +0300

    work 02.11.2021

---
+++ 02.11.2021/cmake-build-debug/CMakeFiles/02_11_2021.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "02_11_2021.exe"
+  "02_11_2021.exe.manifest"
+  "02_11_2021.lib"
+  "02_11_2021.pdb"
+  "CMakeFiles/02_11_2021.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/02_11_2021.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
