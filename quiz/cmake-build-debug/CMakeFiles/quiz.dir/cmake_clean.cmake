2c780221ef8f57363c3a051a72cbdac5d4372eef
GodRayine <NUB999999@gmail.com>
2021-11-12 16:43:17 +0300

    add_quiz_game

---
+++ quiz/cmake-build-debug/CMakeFiles/quiz.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/quiz.dir/main.cpp.obj"
+  "quiz.exe"
+  "quiz.exe.manifest"
+  "quiz.lib"
+  "quiz.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/quiz.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
